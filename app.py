715672dc7cc3f4ded8ac7b44eacda72baafb6ee7
Florian Chazeau <67651522+befloo@users.noreply.github.com>
2025-03-07 14:19:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,6 @@ if uploaded_file:
             files = {"file": ("image.png", img_bytes, "image/png")}
             data = {"style": style_choice}
             response = requests.post(API_URL, headers=headers, files=files, data=data)
-            st.write(response.status_code)
-            st.write(response)
 
 
             if response.status_code == 200:
